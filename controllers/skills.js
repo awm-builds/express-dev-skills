99a2e9ef11016b67286ca01f674bf087d9768a58
awm.builds <81087554+awm-builds@users.noreply.github.com>
2024-04-17 15:48:01 -0400

    -Fixed show functionality

--- controllers/skills.js
+++ controllers/skills.js
@@ -8,14 +8,14 @@
       show
   };
 
-  function index(req, res) {
-    res.render('skills/index', {
-      skills: Skill.getAll()
-    });
+function index(req, res) {
+  res.render('skills/index', {
+    skills: Skill.getAll(),
+  });
 }
-  
+
 function show(req, res) {
   res.render('skills/show', {
-    skills: Skill.getOne(req.params.id),
+    skill: Skill.getOne(req.params.id),
   });
 }
